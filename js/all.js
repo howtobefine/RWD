68cfada498f18b7b223f8d62e854155c387f588e
howtobefine <b4808697b@gmail.com>
2022-06-26 15:41:06 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,9 @@
+// hamburger list
+$(document).ready(function () {
+    $('.list-btn').click(function (e) {
+        e.preventDefault();
+        $('.navbar').toggleClass('menu-list');
+    });
+});
+
+
